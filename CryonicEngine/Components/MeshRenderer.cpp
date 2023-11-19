024cfa9cf0accab4ba0aec8709fd3ae88297d361
Electro Games <2cool4u567@gmail.com>
2023-11-19 17:25:19 -0600

    Added MeshRenderer Component

---
+++ CryonicEngine/Components/MeshRenderer.cpp
@@ -0,0 +1,65 @@
+#include "MeshRenderer.h"
+#include "rlgl.h"
+
+void MeshRenderer::Update(float deltaTime)
+{
+    rlPushMatrix();
+
+    // build up the transform
+    Matrix transform = MatrixTranslate(gameObject.transform.GetPosition().x, gameObject.transform.GetPosition().y, gameObject.transform.GetPosition().z);
+
+    transform = MatrixMultiply(QuaternionToMatrix(gameObject.transform.GetRotation()), transform);
+
+    transform = MatrixMultiply(MatrixScale(gameObject.transform.GetScale().x, gameObject.transform.GetScale().y, gameObject.transform.GetScale().z), transform);
+
+    // apply the transform
+    rlMultMatrixf(MatrixToFloat(transform));
+
+    // draw model
+    if ((std::filesystem::exists(gameObject.GetModelPath())))
+        DrawModel(gameObject.GetModel(), Vector3Zero(), 1, WHITE);
+    else if (gameObject.GetModelPath() == "Cube")
+        DrawCube(Vector3Zero(), 1, 1, 1, LIGHTGRAY);
+    else if (gameObject.GetModelPath() == "Plane")
+        DrawPlane(Vector3Zero(), Vector2{ 1,1 }, LIGHTGRAY);
+    else if (gameObject.GetModelPath() == "Sphere")
+        DrawSphere(Vector3Zero(), 1, LIGHTGRAY);
+    else
+    {
+        // Invalid model path or empty object
+    }
+
+    rlPopMatrix();
+}
+
+
+Model MeshRenderer::GetModel() const
+{
+    return model;
+}
+
+void MeshRenderer::SetModel(Model model)
+{
+    this->model = model;
+    this->bounds = GetMeshBoundingBox(model.meshes[0]); // Todo: Set the correct scale depending on size variable.
+}
+
+std::filesystem::path MeshRenderer::GetModelPath() const
+{
+    return modelPath;
+}
+
+void MeshRenderer::SetModelPath(std::filesystem::path path)
+{
+    this->modelPath = path;
+}
+
+BoundingBox MeshRenderer::GetBounds() const
+{
+    return bounds;
+}
+
+void MeshRenderer::SetBounds(BoundingBox bounds)
+{
+    this->bounds = bounds;
+}
---
+++ CryonicEngine/Components/MeshRenderer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Component.h"
+
+class MeshRenderer : public Component
+{
+public:
+	void Start() override;
+	void Update(float deltaTime) override;
+	void Destroy() override {};
+
+	Model GetModel() const;
+	void SetModel(Model model);
+	std::filesystem::path GetModelPath() const;
+	void SetModelPath(std::filesystem::path path);
+	BoundingBox GetBounds() const;
+	void SetBounds(BoundingBox bounds);
+
+private:
+	Model model;
+	std::filesystem::path modelPath;
+	BoundingBox bounds;
+};
