b132fa1c777d67ec0a383b642c96ac796f2b0f2f
Electro Games <2cool4u567@gmail.com>
2024-01-13 13:58:16 -0600

    Fixed build error

--- CryonicEngine/RaylibInputWrapper.h
+++ CryonicEngine/RaylibInputWrapper.h
@@ -1,13 +1,13 @@
 #pragma once
 
-static bool IsKeyPressedWrapper(int key);
-static bool IsKeyReleasedWrapper(int key);
-static bool IsKeyDownWrapper(int key);
+bool IsKeyPressedWrapper(int key);
+bool IsKeyReleasedWrapper(int key);
+bool IsKeyDownWrapper(int key);
 
-static bool IsMouseButtonPressedWrapper(int button);
-static bool IsMouseButtonReleasedWrapper(int button);
-static bool IsMouseButtonDownWrapper(int button);
+bool IsMouseButtonPressedWrapper(int button);
+bool IsMouseButtonReleasedWrapper(int button);
+bool IsMouseButtonDownWrapper(int button);
 
-static bool IsGamepadButtonPressedWrapper(int button);
-static bool IsGamepadButtonReleasedWrapper(int button);
-static bool IsGamepadButtonDownWrapper(int button);
+bool IsGamepadButtonPressedWrapper(int button);
+bool IsGamepadButtonReleasedWrapper(int button);
+bool IsGamepadButtonDownWrapper(int button);
