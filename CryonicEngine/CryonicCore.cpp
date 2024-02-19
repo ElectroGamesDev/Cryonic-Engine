ca43f71e7c6977ac57458efb54c3fe0e5708014b
Electro Games <2cool4u567@gmail.com>
2024-02-19 12:56:58 -0600

    Added structs and functions

--- CryonicEngine/CryonicCore.cpp
+++ CryonicEngine/CryonicCore.cpp
@@ -1,56 +1,112 @@
 #include "CryonicCore.h"
+#include <cmath>
 
-void Draw3DBillboardRec(Camera camera, Texture2D texture, Rectangle source, Vector3 position, Vector2 size, Color tint)
+Vector3 RotateVector3ByQuaternion(Vector3 vector, Quaternion quaternion)
 {
-    rlPushMatrix();
+    Vector3 result = { 0 };
 
-    // get the camera view matrix
-    Matrix mat = MatrixInvert(MatrixLookAt(camera.position, camera.target, camera.up));
-    // peel off just the rotation
-    Quaternion quat = QuaternionFromMatrix(mat);
-    mat = QuaternionToMatrix(quat);
+    result.x = vector.x * (quaternion.x * quaternion.x + quaternion.w * quaternion.w - quaternion.y * quaternion.y - quaternion.z * quaternion.z) + vector.y * (2 * quaternion.x * quaternion.y - 2 * quaternion.w * quaternion.z) + vector.z * (2 * quaternion.x * quaternion.z + 2 * quaternion.w * quaternion.y);
+    result.y = vector.x * (2 * quaternion.w * quaternion.z + 2 * quaternion.x * quaternion.y) + vector.y * (quaternion.w * quaternion.w - quaternion.x * quaternion.x + quaternion.y * quaternion.y - quaternion.z * quaternion.z) + vector.z * (-2 * quaternion.w * quaternion.x + 2 * quaternion.y * quaternion.z);
+    result.z = vector.x * (-2 * quaternion.w * quaternion.y + 2 * quaternion.x * quaternion.z) + vector.y * (2 * quaternion.w * quaternion.x + 2 * quaternion.y * quaternion.z) + vector.z * (quaternion.w * quaternion.w - quaternion.x * quaternion.x - quaternion.y * quaternion.y + quaternion.z * quaternion.z);
 
-    // apply just the rotation
-    rlMultMatrixf(MatrixToFloat(mat));
-
-    // translate backwards in the inverse rotated matrix to put the item where it goes in world space
-    position = Vector3Transform(position, MatrixInvert(mat));
-    rlTranslatef(position.x, position.y, position.z);
-
-    // draw the billboard
-    float width = size.x / 2;
-    float height = size.y / 2;
-
-    Color color = WHITE;
-
-    rlCheckRenderBatchLimit(6);
-
-    rlSetTexture(texture.id);
-
-    // draw quad
-    rlBegin(RL_QUADS);
-    rlColor4ub(tint.r, tint.g, tint.b, tint.a);
-    // Front Face
-    rlNormal3f(0.0f, 0.0f, 1.0f);                  // Normal Pointing Towards Viewer
-
-    rlTexCoord2f((float)source.x / texture.width, (float)(source.y + source.height) / texture.height);
-    rlVertex3f(-width, -height, 0);  // Bottom Left Of The Texture and Quad
-
-    rlTexCoord2f((float)(source.x + source.width) / texture.width, (float)(source.y + source.height) / texture.height);
-    rlVertex3f(+width, -height, 0);  // Bottom Right Of The Texture and Quad
-
-    rlTexCoord2f((float)(source.x + source.width) / texture.width, (float)source.y / texture.height);
-    rlVertex3f(+width, +height, 0);  // Top Right Of The Texture and Quad
-
-    rlTexCoord2f((float)source.x / texture.width, (float)source.y / texture.height);
-    rlVertex3f(-width, +height, 0);  // Top Left Of The Texture and Quad
-
-    rlEnd();
-    rlSetTexture(0);
-    rlPopMatrix();
+    return result;
 }
 
-void Draw3DBillboard(Camera camera, Texture2D texture, Vector3 position, float size, Color tint)
+Quaternion EulerToQuaternion(float pitch, float yaw, float roll)
 {
-    Draw3DBillboardRec(camera, texture, { 0, 0, (float)texture.width, (float)texture.height }, position, { size, size }, tint);
+    Quaternion result = { 0 };
+
+    float x0 = cosf(pitch * 0.5f);
+    float x1 = sinf(pitch * 0.5f);
+    float y0 = cosf(yaw * 0.5f);
+    float y1 = sinf(yaw * 0.5f);
+    float z0 = cosf(roll * 0.5f);
+    float z1 = sinf(roll * 0.5f);
+
+    result.x = x1 * y0 * z0 - x0 * y1 * z1;
+    result.y = x0 * y1 * z0 + x1 * y0 * z1;
+    result.z = x0 * y0 * z1 - x1 * y1 * z0;
+    result.w = x0 * y0 * z0 + x1 * y1 * z1;
+
+    return result;
 }
+
+
+Vector3 QuaternionToEuler(Quaternion quaternion)
+{
+    Vector3 result = { 0 };
+
+    // Roll (x-axis rotation)
+    float x0 = 2.0f * (quaternion.w * quaternion.x + quaternion.y * quaternion.z);
+    float x1 = 1.0f - 2.0f * (quaternion.x * quaternion.x + quaternion.y * quaternion.y);
+    result.x = atan2f(x0, x1);
+
+    // Pitch (y-axis rotation)
+    float y0 = 2.0f * (quaternion.w * quaternion.y - quaternion.z * quaternion.x);
+    y0 = y0 > 1.0f ? 1.0f : y0;
+    y0 = y0 < -1.0f ? -1.0f : y0;
+    result.y = asinf(y0);
+
+    // Yaw (z-axis rotation)
+    float z0 = 2.0f * (quaternion.w * quaternion.z + quaternion.x * quaternion.y);
+    float z1 = 1.0f - 2.0f * (quaternion.y * quaternion.y + quaternion.z * quaternion.z);
+    result.z = atan2f(z0, z1);
+
+    return result;
+}
+
+
+//void Draw3DBillboardRec(Camera camera, Texture2D texture, Rectangle source, Vector3 position, Vector2 size, Color tint)
+//{
+//    rlPushMatrix();
+//
+//    // get the camera view matrix
+//    Matrix mat = MatrixInvert(MatrixLookAt(camera.position, camera.target, camera.up));
+//    // peel off just the rotation
+//    Quaternion quat = QuaternionFromMatrix(mat);
+//    mat = QuaternionToMatrix(quat);
+//
+//    // apply just the rotation
+//    rlMultMatrixf(MatrixToFloat(mat));
+//
+//    // translate backwards in the inverse rotated matrix to put the item where it goes in world space
+//    position = Vector3Transform(position, MatrixInvert(mat));
+//    rlTranslatef(position.x, position.y, position.z);
+//
+//    // draw the billboard
+//    float width = size.x / 2;
+//    float height = size.y / 2;
+//
+//    Color color = WHITE;
+//
+//    rlCheckRenderBatchLimit(6);
+//
+//    rlSetTexture(texture.id);
+//
+//    // draw quad
+//    rlBegin(RL_QUADS);
+//    rlColor4ub(tint.r, tint.g, tint.b, tint.a);
+//    // Front Face
+//    rlNormal3f(0.0f, 0.0f, 1.0f);                  // Normal Pointing Towards Viewer
+//
+//    rlTexCoord2f((float)source.x / texture.width, (float)(source.y + source.height) / texture.height);
+//    rlVertex3f(-width, -height, 0);  // Bottom Left Of The Texture and Quad
+//
+//    rlTexCoord2f((float)(source.x + source.width) / texture.width, (float)(source.y + source.height) / texture.height);
+//    rlVertex3f(+width, -height, 0);  // Bottom Right Of The Texture and Quad
+//
+//    rlTexCoord2f((float)(source.x + source.width) / texture.width, (float)source.y / texture.height);
+//    rlVertex3f(+width, +height, 0);  // Top Right Of The Texture and Quad
+//
+//    rlTexCoord2f((float)source.x / texture.width, (float)source.y / texture.height);
+//    rlVertex3f(-width, +height, 0);  // Top Left Of The Texture and Quad
+//
+//    rlEnd();
+//    rlSetTexture(0);
+//    rlPopMatrix();
+//}
+//
+//void Draw3DBillboard(Camera camera, Texture2D texture, Vector3 position, float size, Color tint)
+//{
+//    Draw3DBillboardRec(camera, texture, { 0, 0, (float)texture.width, (float)texture.height }, position, { size, size }, tint);
+//}
--- CryonicEngine/CryonicCore.h
+++ CryonicEngine/CryonicCore.h
@@ -1,10 +1,335 @@
 #pragma once
 
-#include "raylib.h"
-#include "raymath.h"
-#include "rlgl.h"
-#include <stddef.h>
+//void Draw3DBillboardRec(Camera camera, Texture2D texture, Rectangle source, Vector3 position, Vector2 size, Color tint);
 
-void Draw3DBillboardRec(Camera camera, Texture2D texture, Rectangle source, Vector3 position, Vector2 size, Color tint);
+//void Draw3DBillboard(Camera camera, Texture2D texture, Vector3 position, float size, Color tint);
 
-void Draw3DBillboard(Camera camera, Texture2D texture, Vector3 position, float size, Color tint);
+struct Color {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
+};
+
+struct Texture {
+    unsigned int id;        // OpenGL texture id
+    int width;              // Texture base width
+    int height;             // Texture base height
+    int mipmaps;            // Mipmap levels, 1 by default
+    int format;             // Data format (PixelFormat type)
+};
+
+typedef Texture Texture2D;
+
+const static Color RED = { 255, 0, 0, 255 };
+const static Color GREEN = { 0, 255, 0, 255 };
+const static Color BLUE = { 0, 0, 255, 255 };
+const static Color YELLOW = { 255, 255, 0, 255 };
+const static Color ORANGE = { 255, 165, 0, 255 };
+const static Color PURPLE = { 128, 0, 128, 255 };
+const static Color BLACK = { 0, 0, 0, 255 };
+const static Color WHITE = { 255, 255, 255, 255 };
+const static Color GRAY = { 128, 128, 128, 255 };
+const static Color DARKGRAY = { 64, 64, 64, 255 };
+const static Color LIGHTGRAY = { 192, 192, 192, 255 };
+const static Color BEIGE = { 245, 245, 220, 255 };
+const static Color BROWN = { 165, 42, 42, 255 };
+const static Color MAROON = { 128, 0, 0, 255 };
+const static Color GOLD = { 255, 215, 0, 255 };
+const static Color LIME = { 0, 255, 0, 255 };
+const static Color PINK = { 255, 192, 203, 255 };
+const static Color DARKBLUE = { 0, 0, 139, 255 };
+const static Color MAGENTA = { 255, 0, 255, 255 };
+const static Color SKYBLUE = { 135, 206, 235, 255 };
+const static Color VIOLET = { 238, 130, 238, 255 };
+
+struct Vector2
+{
+    float x;
+    float y;
+
+    Vector2(float x = 0.0f, float y = 0.0f) : x(x), y(y) {}
+
+    Vector2 operator+(const Vector2& other) const {
+        return { x + other.x, y + other.y };
+    }
+
+    Vector2 operator-(const Vector2& other) const {
+        return { x - other.x, y - other.y };
+    }
+
+    Vector2 operator*(float scalar) const {
+        return { x * scalar, y * scalar };
+    }
+
+    Vector2 operator/(float scalar) const {
+        if (scalar != 0.0f)
+            return { x / scalar, y / scalar };
+        else
+            return { 0.0f, 0.0f }; // Todo: Return error
+    }
+
+    Vector2 operator*(const Vector2& other) const {
+        return { x * other.x, y * other.y };
+    }
+
+    Vector2 operator/(const Vector2& other) const {
+        if (other.x != 0.0f && other.y != 0.0f)
+            return { x / other.x, y / other.y };
+        else
+            return { 0.0f, 0.0f }; // Todo: Return error
+    }
+
+    Vector2& operator+=(const Vector2& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    Vector2& operator-=(const Vector2& other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    Vector2& operator*=(float scalar) {
+        x *= scalar;
+        y *= scalar;
+        return *this;
+    }
+
+    Vector2& operator/=(float scalar) {
+        if (scalar != 0.0f) {
+            x /= scalar;
+            y /= scalar;
+        }
+        return *this;
+    }
+
+    Vector2& operator*=(const Vector2& other) {
+        x *= other.x;
+        y *= other.y;
+        return *this;
+    }
+
+    Vector2& operator/=(const Vector2& other) {
+        if (other.x != 0.0f && other.y != 0.0f) {
+            x /= other.x;
+            y /= other.y;
+        }
+        return *this;
+    }
+
+    bool operator==(const Vector2& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    bool operator!=(const Vector2& other) const {
+        return !(*this == other);
+    }
+};
+
+
+struct Vector3
+{
+	float x;
+	float y;
+    float z;
+
+    Vector3(float x = 0.0f, float y = 0.0f, float z = 0.0f) : x(x), y(y), z(z) {}
+
+    Vector3 operator+(const Vector3& other) const {
+        return { x + other.x, y + other.y, z + other.z };
+    }
+
+    Vector3 operator-(const Vector3& other) const {
+        return { x - other.x, y - other.y, z - other.z };
+    }
+
+    Vector3 operator*(float scalar) const {
+        return { x * scalar, y * scalar, z * scalar };
+    }
+    Vector3 operator*(const Vector3& other) const {
+        return { x * other.x, y * other.y, z * other.z };
+    }
+
+    Vector3 operator/(float scalar) const {
+        if (scalar != 0.0f)
+            return { x / scalar, y / scalar, z / scalar };
+        else
+            return { 0.0f, 0.0f, 0.0f }; // Todo: Send error
+    }
+
+    Vector3 operator/(const Vector3& other) const {
+        if (other.x == 0 || other.y == 0 || other.z == 0) {
+            // Todo: Send error
+            return { 0.0f, 0.0f, 0.0f };
+        }
+
+        return { x / other.x, y / other.y, z / other.z };
+    }
+
+    Vector3& operator+=(const Vector3& other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+    }
+
+    Vector3& operator-=(const Vector3& other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        return *this;
+    }
+
+    Vector3& operator*=(float scalar) {
+        x *= scalar;
+        y *= scalar;
+        z *= scalar;
+        return *this;
+    }
+
+    Vector3& operator*=(const Vector3& other) {
+        x *= other.x;
+        y *= other.y;
+        z *= other.z;
+        return *this;
+    }
+
+    Vector3& operator/=(float scalar) {
+        if (scalar != 0.0f) {
+            x /= scalar;
+            y /= scalar;
+            z /= scalar;
+        }
+        return *this;
+    }
+
+    Vector3& operator/=(const Vector3& other) {
+        if (other.x != 0.0f && other.y != 0.0f && other.z != 0.0f) {
+            x /= other.x;
+            y /= other.y;
+            z /= other.z;
+        }
+        return *this;
+    }
+
+    bool operator==(const Vector3& other) const {
+        return x == other.x && y == other.y && z == other.z;
+    }
+
+    bool operator!=(const Vector3& other) const {
+        return !(*this == other);
+    }
+};
+
+struct Vector4 {
+    float x;
+    float y;
+    float z;
+    float w;
+
+    Vector4 operator+(const Vector4& other) const {
+        return { x + other.x, y + other.y, z + other.z, w + other.w };
+    }
+
+    Vector4 operator-(const Vector4& other) const {
+        return { x - other.x, y - other.y, z - other.z, w - other.w };
+    }
+
+    Vector4 operator*(float scalar) const {
+        return { x * scalar, y * scalar, z * scalar, w * scalar };
+    }
+
+    Vector4 operator*(const Vector4& other) const {
+        return { x * other.x, y * other.y, z * other.z, w * other.w };
+    }
+
+    Vector4 operator/(float scalar) const {
+        if (scalar != 0.0f) {
+            return { x / scalar, y / scalar, z / scalar, w / scalar };
+        }
+        else
+            return { 0.0f, 0.0f, 0.0f, 0.0f }; // Todo: Send error
+    }
+
+    Vector4 operator/(const Vector4& other) const {
+        if (other.x == 0 || other.y == 0 || other.z == 0 || other.w == 0)
+            return { 0.0f, 0.0f, 0.0f }; // Todo: Send error
+
+        return { x / other.x, y / other.y, z / other.z, w / other.w };
+    }
+
+    Vector4& operator+=(const Vector4& other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        w += other.w;
+        return *this;
+    }
+
+    Vector4& operator-=(const Vector4& other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        w -= other.w;
+        return *this;
+    }
+
+    Vector4& operator*=(float scalar) {
+        x *= scalar;
+        y *= scalar;
+        z *= scalar;
+        w *= scalar;
+        return *this;
+    }
+
+    Vector4& operator/=(float scalar) {
+        if (scalar != 0.0f) {
+            x /= scalar;
+            y /= scalar;
+            z /= scalar;
+            w /= scalar;
+        }
+        return *this;
+    }
+
+    Vector4& operator*=(const Vector4& other) {
+        x *= other.x;
+        y *= other.y;
+        z *= other.z;
+        w *= other.w;
+        return *this;
+    }
+
+    Vector4& operator/=(const Vector4& other) {
+        if (other.x != 0.0f && other.y != 0.0f && other.z != 0.0f && other.w != 0.0f) {
+            x /= other.x;
+            y /= other.y;
+            z /= other.z;
+            w /= other.w;
+        }
+        return *this;
+    }
+
+    bool operator==(const Vector4& other) const {
+        return x == other.x && y == other.y && z == other.z && w == other.w;
+    }
+
+    bool operator!=(const Vector4& other) const {
+        return !(*this == other);
+    }
+
+    static Vector4 Identity()
+    {
+        return { 0.0f, 0.0f, 0.0f, 1.0f };
+    }
+};
+
+typedef Vector4 Quaternion;
+
+Vector3 RotateVector3ByQuaternion(Vector3 vector, Quaternion quaternion);
+Quaternion EulerToQuaternion(float roll, float pitch, float yaw);
+//Returns Vector3 in Radians.
+Vector3 QuaternionToEuler(Quaternion quaternion);
