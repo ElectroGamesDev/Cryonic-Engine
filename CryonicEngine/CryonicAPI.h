f759cc8d508ff59af48471fde8fb3fed53c70502
Electro Games <2cool4u567@gmail.com>
2024-02-19 12:57:31 -0600

    Added InputSystem

--- CryonicEngine/CryonicAPI.h
+++ CryonicEngine/CryonicAPI.h
@@ -3,7 +3,10 @@
 #include "ConsoleLogger.h"
 #include "GameObject.h"
 #include "Scenes/SceneManager.h"
-//#include "InputSystem.h"
+#include "InputSystem.h"
 
 #include "Components/Component.h"
 #include "Components/MeshRenderer.h"
+#include "Components/CameraComponent.h"
+#include "Components/Lighting.h"
+#include "Components/SpriteRenderer.h"
