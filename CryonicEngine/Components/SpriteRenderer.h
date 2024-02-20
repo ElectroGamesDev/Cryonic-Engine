6ab79dd2b7af89e27e222a0ecf6caf20720d8062
Electro Games <2cool4u567@gmail.com>
2024-02-19 22:25:28 -0600

    Fixed crash

--- CryonicEngine/Components/SpriteRenderer.h
+++ CryonicEngine/Components/SpriteRenderer.h
@@ -13,25 +13,25 @@ public:
 		name = "SpriteRenderer";
 		iconUnicode = "\xef\x87\x85";
 
-#if defined(EDITOR)
-		nlohmann::json jsonArray = nlohmann::json::array();
-		jsonArray.push_back(0);
-
-		nlohmann::json variablesArray = nlohmann::json::array();
-		variablesArray.push_back("Color");
-		variablesArray.push_back("tint");
-		nlohmann::json colorArray = nlohmann::json::array();
-		colorArray.push_back("255");
-		colorArray.push_back("255");
-		colorArray.push_back("255");
-		colorArray.push_back("255");
-		variablesArray.push_back(colorArray);
-		variablesArray.push_back("Tint");
-
-		jsonArray.push_back(variablesArray);
-
-		exposedVariables = jsonArray;
-#endif
+//#if defined(EDITOR)
+//		nlohmann::json jsonArray = nlohmann::json::array();
+//		jsonArray.push_back(0);
+//
+//		nlohmann::json variablesArray = nlohmann::json::array();
+//		variablesArray.push_back("Color");
+//		variablesArray.push_back("tint");
+//		nlohmann::json colorArray = nlohmann::json::array();
+//		colorArray.push_back("255");
+//		colorArray.push_back("255");
+//		colorArray.push_back("255");
+//		colorArray.push_back("255");
+//		variablesArray.push_back(colorArray);
+//		variablesArray.push_back("Tint");
+//
+//		jsonArray.push_back(variablesArray);
+//
+//		exposedVariables = jsonArray;
+//#endif
 	}
 	// Hide everything from API
 	void Start() override {};
