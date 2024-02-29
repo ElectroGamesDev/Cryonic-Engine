ef02d27965e4d35abe313ae6d1e4028a2cb8457a
Electro Games <2cool4u567@gmail.com>
2024-02-28 18:54:55 -0600

    Fixed Rigidbody with SetPosition() and SetRotation()

--- CryonicEngine/Components/Rigidbody2D.cpp
+++ CryonicEngine/Components/Rigidbody2D.cpp
@@ -2,15 +2,44 @@
 #if !defined(EDITOR)
 #include "../box2d/box2d.h"
 #include "../Game.h"
+
+Vector3 lastGameObjectPosition;
+Quaternion lastGameObjectRotation;
+
 #endif
 
 
 Rigidbody2D::Rigidbody2D(GameObject* obj) : Component(obj) {
 	name = "Rigidbody2D";
+#if defined(EDITOR)
+    std::string variables = R"(
+        [
+            0,
+            [
+                [
+                    "int",
+                    "bodyType",
+                    0,
+                    "Body Type"
+                ]
+            ]
+        ]
+    )";
+    exposedVariables = nlohmann::json::parse(variables);
+#endif
 
 #if !defined(EDITOR)
+    lastGameObjectPosition = gameObject->transform.GetPosition();
+    lastGameObjectRotation = gameObject->transform.GetRotation();
+
 	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
+    // Todo: Change this to a switch case
+    if (bodyType == 0)
+	    bodyDef.type = b2_dynamicBody;
+    else if (bodyType == 1)
+        bodyDef.type = b2_kinematicBody;
+    else
+        bodyDef.type = b2_staticBody;
 	bodyDef.position.Set(gameObject->transform.GetPosition().x, gameObject->transform.GetPosition().y);
 	body = world->CreateBody(&bodyDef);
 #endif
@@ -18,9 +47,38 @@ Rigidbody2D::Rigidbody2D(GameObject* obj) : Component(obj) {
 
 void Rigidbody2D::Update(float deltaTime)
 {
-	// This is probably not a good solution since then scripts can't set gameobject position by gameObject->transform.SetPositon
 #if !defined(EDITOR)
-	gameObject->transform.SetPosition({ body->GetPosition().x, body->GetPosition().y, 0});
-	gameObject->transform._rotation.y = body->GetAngle() * RAD2DEG;
+    // Todo: Change this to a switch case
+    if (bodyType == 0)
+    {
+        if (gameObject->transform.GetPosition() == lastGameObjectPosition)
+            gameObject->transform.SetPosition({ body->GetPosition().x, body->GetPosition().y, 0 });
+        else
+            body->SetTransform({ gameObject->transform.GetPosition().x, gameObject->transform.GetPosition().y }, body->GetAngle());
+
+        if (gameObject->transform.GetRotation() == lastGameObjectRotation)
+            gameObject->transform._rotation.y = body->GetAngle() * RAD2DEG;
+        else
+            body->SetTransform(body->GetPosition(), DEG2RAD * gameObject->transform.GetRotation().y);
+
+        lastGameObjectPosition = gameObject->transform.GetPosition();
+        lastGameObjectRotation = gameObject->transform.GetRotation();
+    }
+    else if (bodyType == 1)
+    {
+        if (gameObject->transform.GetPosition() != lastGameObjectPosition)
+            body->SetTransform({ gameObject->transform.GetPosition().x, gameObject->transform.GetPosition().y }, body->GetAngle());
+
+        if (gameObject->transform.GetRotation() != lastGameObjectRotation)
+            body->SetTransform(body->GetPosition(), DEG2RAD * gameObject->transform.GetRotation().y);
+
+        lastGameObjectPosition = gameObject->transform.GetPosition();
+        lastGameObjectRotation = gameObject->transform.GetRotation();
+    }
+    else if (bodyType == 2)
+    {
+        gameObject->transform.SetPosition(lastGameObjectPosition);
+        gameObject->transform._rotation.y = lastGameObjectRotation.y;
+    }
 #endif
 }
--- CryonicEngine/Components/Rigidbody2D.h
+++ CryonicEngine/Components/Rigidbody2D.h
@@ -11,6 +11,10 @@ public:
     Rigidbody2D(GameObject* obj);
     void Update(float deltaTime) override;
 
+    // Temporary solution, remove this
+    int bodyType = 0;
+    // Todo: Add an enum to select body type. Maybe do this by when looking in header if it finds "enum", it will get the next word knowing that is an enum variable type so then it can detect enums as an exposedVariable
+
     // Hide in API
 #if !defined(EDITOR)
     b2Body* body = nullptr;
