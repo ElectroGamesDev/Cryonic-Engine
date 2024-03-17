d7d28a0c54d6b56c75211f4fb4c49567e46ded35
Electro Games <2cool4u567@gmail.com>
2024-03-17 14:54:49 -0500

    Added comment

--- CryonicEngine/ShaderManager.cpp
+++ CryonicEngine/ShaderManager.cpp
@@ -23,6 +23,7 @@ void ShaderManager::Init()
     //shaders[LitStandard] = LoadShader(("resources/shaders/glsl" + std::to_string(GLSL_VERSION) + "/lighting.vs").c_str(), ("resources/shaders/glsl" + std::to_string(GLSL_VERSION) + "/lighting.fs").c_str());
 
     // Using __FILE__ since if I don't, game builds will get the path of the .exe and not ShaderManger.cpp file
+    // Todo: Replace glsl330 to GLSL_VERSION. I will need to define the platform though
     RaylibShader::shaders[LitStandard].Load((std::filesystem::path(__FILE__).parent_path() / "resources/shaders/glsl330/lighting.vs").string().c_str(), (std::filesystem::path(__FILE__).parent_path() / "resources/shaders/glsl330/lighting.fs").string().c_str());
 
     //std::string currentDirectory = GetWorkingDirectory();
