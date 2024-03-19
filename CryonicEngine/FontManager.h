1c7861288db2eaa80556160c030c37f0f4357ce1
Electro Games <2cool4u567@gmail.com>
2024-03-18 23:17:26 -0500

    Changed class to namespace

--- CryonicEngine/FontManager.h
+++ CryonicEngine/FontManager.h
@@ -8,9 +8,8 @@
 //#include "rlImGui.h"
 #include "imgui.h"
 
-class FontManager
+namespace FontManager
 {
-public:
     static ImFont* CreateFont(std::string font, int size, bool iconFont = false);
     static ImFont* GetFont(std::string font, int size, bool checkIfExists = true);
     static void CreateFonts(std::string font, std::vector<int> sizes);
