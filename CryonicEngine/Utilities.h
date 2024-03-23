ca123143f18b51916b0839d2dcc98613b4065944
Electro Games <2cool4u567@gmail.com>
2024-03-22 21:32:20 -0500

    Changed from class to namespace

--- CryonicEngine/Utilities.h
+++ CryonicEngine/Utilities.h
@@ -3,12 +3,12 @@
 #include <filesystem>
 #include <json.hpp>
 
-class Utilities {
-public:
-    static void OpenPathInExplorer(std::filesystem::path path);
-    static std::string SelectFolderDialog(const std::filesystem::path& projectPath);
-    static void HideFile(std::filesystem::path path);
-    static int GetNumberOfCores();
-    static std::filesystem::path CreateUniqueFile(std::filesystem::path path, std::string name, std::string extension);
-    static nlohmann::json GetExposedVariables(std::filesystem::path path);
+namespace Utilities
+{
+    void OpenPathInExplorer(std::filesystem::path path);
+    std::string SelectFolderDialog(const std::filesystem::path& projectPath);
+    void HideFile(std::filesystem::path path);
+    int GetNumberOfCores();
+    std::filesystem::path CreateUniqueFile(std::filesystem::path path, std::string name, std::string extension);
+    nlohmann::json GetExposedVariables(std::filesystem::path path);
 };
