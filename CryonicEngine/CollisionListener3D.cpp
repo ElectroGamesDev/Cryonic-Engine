fff3429e527bedb1ad231e9a0712cdc12bbe81ee
Electro Games <2cool4u567@gmail.com>
2024-03-25 14:51:52 -0500

    Added CollisionListener3D

---
+++ CryonicEngine/CollisionListener3D.cpp
@@ -0,0 +1,54 @@
+#include "CollisionListener3D.h"
+#include "Components/Collider3D.h"
+
+std::unordered_map<std::pair<BodyID, BodyID>, std::pair<Collider3D*, Collider3D*>> contactMap;
+
+void CollisionListener3D::OnContactAdded(const Body& inBody1, const Body& inBody2, const ContactManifold& inManifold, ContactSettings& ioSettings)
+{
+    Collider3D* colliderA = reinterpret_cast<Collider3D*>(inBody1.GetUserData());
+    Collider3D* colliderB = reinterpret_cast<Collider3D*>(inBody2.GetUserData());
+
+    contactMap[std::make_pair(inBody1.GetID(), inBody2.GetID())] = std::make_pair(colliderA, colliderB);
+
+    if (colliderA && colliderB)
+    {
+        for (Component* component : colliderA->gameObject->GetComponents())
+            component->OnCollisionEnter3D(colliderB);
+        for (Component* component : colliderB->gameObject->GetComponents())
+            component->OnCollisionEnter3D(colliderA);
+    }
+}
+
+void CollisionListener3D::OnContactPersisted(const Body& inBody1, const Body& inBody2, const ContactManifold& inManifold, ContactSettings& ioSettings)
+{
+    Collider3D* colliderA = reinterpret_cast<Collider3D*>(inBody1.GetUserData());
+    Collider3D* colliderB = reinterpret_cast<Collider3D*>(inBody2.GetUserData());
+
+    if (colliderA && colliderB)
+    {
+        for (Component* component : colliderA->gameObject->GetComponents())
+            component->OnCollisionExit3D(colliderB);
+        for (Component* component : colliderB->gameObject->GetComponents())
+            component->OnCollisionExit3D(colliderA);
+    }
+}
+
+void CollisionListener3D::OnContactRemoved(const SubShapeIDPair& inSubShapePair)
+{
+    if (auto it = contactMap.find(std::make_pair(inSubShapePair.GetBody1ID(), inSubShapePair.GetBody2ID())); it != contactMap.end())
+    {
+        Collider3D* colliderA = it->second.first;
+        Collider3D* colliderB = it->second.second;
+
+        // Checking if they're null separately since they should still callback even if the other collider is now null. Components are responsible for ensuring they're valid
+        if (colliderA)
+            for (Component* component : colliderA->gameObject->GetComponents())
+                component->OnCollisionStay3D(colliderB);
+
+        if (colliderB)
+            for (Component* component : colliderB->gameObject->GetComponents())
+                component->OnCollisionStay3D(colliderA);
+
+        contactMap.erase(it);
+    }
+}
---
+++ CryonicEngine/CollisionListener3D.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// Todo: Remove unnecessary includes also, move these includes over to Collider3D.h, remove the Collider3D.h include from CollisionListener3D.cpp, and move it here.
+#include "Jolt/Jolt.h"
+#include "Jolt/RegisterTypes.h"
+#include "Jolt/Core/Factory.h"
+#include "Jolt/Core/TempAllocator.h"
+#include "Jolt/Core/JobSystemThreadPool.h"
+#include "Jolt/Physics/PhysicsSettings.h"
+#include "Jolt/Physics/PhysicsSystem.h"
+#include "Jolt/Physics/Collision/Shape/BoxShape.h"
+#include "Jolt/Physics/Collision/Shape/SphereShape.h"
+#include "Jolt/Physics/Body/BodyCreationSettings.h"
+#include "Jolt/Physics/Body/BodyActivationListener.h"
+
+using namespace JPH;
+
+class CollisionListener3D : public ContactListener {
+public:
+	virtual void OnContactAdded(const Body& inBody1, const Body& inBody2, const ContactManifold& inManifold, ContactSettings& ioSettings) override;
+	virtual void OnContactPersisted(const Body& inBody1, const Body& inBody2, const ContactManifold& inManifold, ContactSettings& ioSettings) override;
+	virtual void OnContactRemoved(const SubShapeIDPair& inSubShapePair) override;
+};
