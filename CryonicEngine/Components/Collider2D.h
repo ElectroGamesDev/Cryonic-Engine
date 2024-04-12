afc4b1dcb914652b5484273ceb7209eae1b2ce2b
Electro Games <2cool4u567@gmail.com>
2024-04-12 13:15:21 -0500

    Added GetShape(), SetTrigger(), IsTrigger(). Also moved some variables from the .cpp to the header

--- CryonicEngine/Components/Collider2D.h
+++ CryonicEngine/Components/Collider2D.h
@@ -2,13 +2,29 @@
 
 #include "Component.h"
 
+enum Shape
+{
+    Square,
+    Circle
+};
+
 class Collider2D : public Component {
 public:
     Collider2D(GameObject* obj, int id);
     void Start() override;
     void Destroy() override;
 
-    // Render gizmos in Update()
+    Shape GetShape();
+    void SetTrigger(bool value);
+    bool IsTrigger();
 
-    // Todo: Create an enum to select the type of collider for example; Box, Circle, Polygon/Convex, etc. Or maybe even an Array would work as it would be easier to detect as a variable
+private:
+    Shape shape;
+    bool trigger;
+
+#if !defined(EDITOR)
+    b2FixtureDef fixtureDef;
+    b2Body* body;
+    b2Fixture fixture;
+#endif
 };
