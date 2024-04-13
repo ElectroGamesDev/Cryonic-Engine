3c9e3a501de8080000762b51d727a0db90664a44
Electro Games <2cool4u567@gmail.com>
2024-04-12 20:23:21 -0500

    Added Destroy()

--- CryonicEngine/Components/CameraComponent.h
+++ CryonicEngine/Components/CameraComponent.h
@@ -10,6 +10,7 @@ public:
 	CameraComponent(GameObject* obj, int id);
 	void Start() override;
 	void Update(float deltaTime) override;
+	void Destroy() override;
 #ifdef EDITOR
 	void EditorUpdate() override;
 #endif
