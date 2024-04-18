336392ee6c8c8fc09301dfd23c0e46ed2a6b0369
Electro Games <2cool4u567@gmail.com>
2024-04-17 23:39:52 -0500

    Added support for no parameter events. unlimited parameter events, and function pointer events

--- CryonicEngine/EventSystem.cpp
+++ CryonicEngine/EventSystem.cpp
@@ -1,10 +1,24 @@
 #include "EventSystem.h"
 
-void EventSystem::Subscribe(const std::string& eventName, EventHandler handler) {
-    eventHandlers[eventName].push_back(handler);
+std::unordered_map<std::string, std::vector<EventSystem::EventHandler>> eventHandlers;
+
+std::vector<EventSystem::EventHandler>& EventSystem::GetHandlers(const std::string& eventName) {
+    return eventHandlers[eventName];
 }
 
-void EventSystem::Invoke(const std::string& eventName, void* eventData) {
-    for (auto& handler : eventHandlers[eventName])
-        handler(eventData);
+void EventSystem::Subscribe(const std::string& eventName, EventHandler handler) {
+    GetHandlers(eventName).push_back(handler);
+}
+
+void EventSystem::Subscribe(const std::string& eventName, void(*handler)(void*)) {
+    EventHandler wrapper = [handler](void* data) {
+        handler(data);
+    };
+    GetHandlers(eventName).push_back(wrapper);
+}
+
+void EventSystem::Invoke(const std::string& eventName) {
+    for (auto& handler : GetHandlers(eventName)) {
+        handler(nullptr);
+    }
 }
--- CryonicEngine/EventSystem.h
+++ CryonicEngine/EventSystem.h
@@ -9,9 +9,32 @@ class EventSystem {
 public:
     using EventHandler = std::function<void(void*)>;
 
+    template<typename Func>
+    static void Subscribe(const std::string& eventName, Func&& handler) {
+        SubscribeImpl(eventName, std::forward<Func>(handler), std::make_index_sequence<std::tuple_size_v<std::decay_t<Func>>>{});
+    }
+
     static void Subscribe(const std::string& eventName, EventHandler handler);
-    static void Invoke(const std::string& eventName, void* eventData);
+
+    static void Subscribe(const std::string& eventName, void(*handler)(void*));
+
+    template<typename... Args>
+    static void Invoke(const std::string& eventName, Args&&... args) {
+        for (auto& handler : GetHandlers(eventName)) {
+            handler(&std::make_tuple(std::forward<Args>(args)...));
+        }
+    }
+
+    static void Invoke(const std::string& eventName);
 
 private:
-    static std::unordered_map<std::string, std::vector<EventHandler>> eventHandlers;
+    template<typename Func, std::size_t... Is>
+    static void SubscribeImpl(const std::string& eventName, Func&& handler, std::index_sequence<Is...>) {
+        auto wrapper = [=](void* data) {
+            handler(*reinterpret_cast<std::tuple_element_t<Is, std::decay_t<Func>>*>(data)...);
+        };
+        GetHandlers(eventName).push_back(wrapper);
+    }
+
+    static std::vector<EventHandler>& GetHandlers(const std::string& eventName);
 };
