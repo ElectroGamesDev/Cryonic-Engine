7d3971c8c1c5e4ad5356fd2a94960f86c0af2aa6
Electro Games <2cool4u567@gmail.com>
2024-05-03 00:07:09 -0500

    Added animationGraph exposed variable

--- CryonicEngine/Components/AnimationPlayer.h
+++ CryonicEngine/Components/AnimationPlayer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Component.h"
+#include "../AnimationGraph.h"
 
 class AnimationPlayer : public Component
 {
@@ -9,11 +10,31 @@ public:
 	{
 		name = "AnimationPlayer";
 		iconUnicode = "\xef\x9c\x8c";
+
+#if defined(EDITOR)
+        std::string variables = R"(
+        [
+            0,
+            [
+                [
+                    "AnimationGraph",
+                    "animationGraph",
+                    "nullptr",
+                    "Animation Graph",
+                    "Extensions": [".animgraph"]
+                ]
+            ]
+        ]
+    )";
+        exposedVariables = nlohmann::json::parse(variables);
+#endif
 	}
 
 	void Start() override {};
 	void Update(float deltaTime) override;
 	void Destroy() override;
+
+    AnimationGraph* animationGraph = nullptr;
 
 private:
 };
