bf34eba5d42da27c565f2be5a37f1619ed259751
Electro Games <2cool4u567@gmail.com>
2024-05-02 22:58:13 -0500

    Added missing component includes

--- CryonicEngine/CryonicAPI.h
+++ CryonicEngine/CryonicAPI.h
@@ -7,9 +7,13 @@
 #include "EventSystem.h"
 
 #include "Components/Component.h"
+#include "Components/AnimationPlayer.h"
 #include "Components/MeshRenderer.h"
 #include "Components/CameraComponent.h"
 #include "Components/Lighting.h"
 #include "Components/SpriteRenderer.h"
 #include "Components/Collider2D.h"
+#include "Components/Collider3D.h"
 #include "Components/Rigidbody2D.h"
+#include "Components/Rigidbody3D.h"
+#include "Components/Skybox.h"
