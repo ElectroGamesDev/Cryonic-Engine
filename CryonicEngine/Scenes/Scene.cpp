c32b127f81f68147a566c0c7691a024089ee19fe
Electro Games <2cool4u567@gmail.com>
2024-05-21 12:47:53 -0500

    Added SpawnGameObject() and SpawnObject()

--- CryonicEngine/Scenes/Scene.cpp
+++ CryonicEngine/Scenes/Scene.cpp
@@ -1,6 +1,10 @@
 #include "Scene.h"
 #include <algorithm>
 #include "../Components/Component.h"
+#include "../Components/SpriteRenderer.h"
+#if defined(EDITOR)
+#include "../ProjectManager.h"
+#endif
 
 Scene::Scene(const std::filesystem::path& path, std::deque<GameObject*> gameObjects)
     : m_Path(path), m_GameObjects(gameObjects)
@@ -68,4 +72,150 @@ GameObject* Scene::GetGameObject(const std::string& name)
         if (gameObject->GetName() == name)
             return gameObject;
     return nullptr;
+}
+
+// Todo: Add Asset Template support for SpawnGameObject
+
+GameObject* Scene::SpawnGameObject(std::string path, Vector3 position, Quaternion rotation)
+{
+    std::filesystem::path newPath = path;
+#if !defined(EDITOR)
+    newPath = std::filesystem::path(RaylibWrapper::GetWorkingDirectory()) / "Resources" / "Assets" / newPath;
+#else
+    newPath = ProjectManager::projectData.path / "Assets" / newPath;
+#endif
+
+    if (!newPath.has_extension() || (newPath.extension() == ".png" || newPath.extension() == ".jpg" || newPath.extension() == ".jpeg"))
+    {
+        ConsoleLogger::WarningLog("Failed to spawn a game object. The specified file must be a PNG, JPG, or JPEG. specified path: " + newPath.string());
+        return nullptr;
+    }
+
+    if (!std::filesystem::exists(newPath))
+    {
+        ConsoleLogger::WarningLog("Failed to spawn a game object. Invalid sprite/template path. specified path: " + newPath.string());
+        return nullptr;
+    }
+
+    GameObject* gameObject = AddGameObject();
+    gameObject->transform.SetPosition(position);
+    gameObject->transform.SetScale({ 1,1,1 });
+    gameObject->transform.SetRotation(rotation);
+    gameObject->SetName(newPath.stem().string());
+
+    SpriteRenderer& spriteRenderer = gameObject->AddComponent<SpriteRenderer>();
+    spriteRenderer.SetTexture(newPath);
+    spriteRenderer.gameObject = gameObject;
+
+    return gameObject;
+}
+
+GameObject* Scene::SpawnGameObject(std::string path)
+{
+    return SpawnGameObject(path, { 0,0,0 }, Quaternion::Identity());
+}
+
+GameObject* Scene::SpawnGameObject(std::string path, Vector3 position)
+{
+    return SpawnGameObject(path, position, Quaternion::Identity());
+
+}
+
+GameObject* Scene::SpawnGameObject(std::string path, Vector3 position, Vector3 rotation)
+{
+    return SpawnGameObject(path, position, EulerToQuaternion(rotation.x * DEG2RAD, rotation.y * DEG2RAD, rotation.z * DEG2RAD));
+}
+
+GameObject* Scene::SpawnGameObject(GameObject* gameObject, Vector3 position, Quaternion rotation) // INCOMPLETE
+{
+    if (gameObject == nullptr)
+    {
+        ConsoleLogger::WarningLog("Failed to spawn a game object. Invalid Game Object to clone.");
+        return nullptr;
+    }
+
+    GameObject* clonedGameObject = AddGameObject();
+    clonedGameObject->transform.SetPosition(position);
+    clonedGameObject->transform.SetScale(gameObject->transform.GetScale());
+    clonedGameObject->transform.SetRotation(rotation);
+    clonedGameObject->SetName(gameObject->GetName());
+
+    // Todo: Clone children & set the parent of the children & reset the current children list and add these children to it. Just use SpawnGameObject() to clone children
+
+    for (auto& component : gameObject->GetComponents())
+    {
+        // Todo: if an exposed variable or variable is set to a gameobject or component on this gameobject or a child, then set it to the cloned one
+        // Todo: This will cause issues if it tries to call Clone() on an external script.
+        // Todo: Check who owns the new cloned component, I think the original Component might
+
+        // Todo: Inside of each of the Clone() functions it needs to set the variables, but I need to figure out how to do that while letting components vector own the Component
+
+        clonedGameObject->AddClonedComponent(component);
+
+        // Make this work in builds. So don't clone custom components, just internal & ScriptComponent. Then run the SetupScriptComponent()
+        //  It may be best just to Add Clone() to custom components when building them. BuildScripts() in ScriptLoader() does make sense to use to put it in, but ExposedVariables already has most of the code.
+        // Only need to set exposed variables
+        // Make sure to call SetExposedVariables if not in Editor
+        // Get component and set gameObject
+    }
+
+    //for (Component* component : SceneManager::GetActiveScene()->GetGameObjects().back()->GetComponents())
+    //    component->gameObject = SceneManager::GetActiveScene()->GetGameObjects().back();
+
+    return clonedGameObject;
+}
+
+GameObject* Scene::SpawnGameObject(GameObject* gameObject)
+{
+    return SpawnGameObject(gameObject, gameObject->transform.GetPosition(), gameObject->transform.GetRotation());
+}
+
+GameObject* Scene::SpawnGameObject(GameObject* gameObject, Vector3 position)
+{
+    return SpawnGameObject(gameObject, position, gameObject->transform.GetRotation());
+}
+
+GameObject* Scene::SpawnGameObject(GameObject* gameObject, Vector3 position, Vector3 rotation)
+{
+    return SpawnGameObject(gameObject, position, EulerToQuaternion(rotation.x * DEG2RAD, rotation.y * DEG2RAD, rotation.z * DEG2RAD));
+}
+
+GameObject* Scene::SpawnObject(std::string path)
+{
+    return SpawnGameObject(path);
+}
+
+GameObject* Scene::SpawnObject(std::string path, Vector3 position)
+{
+    return SpawnGameObject(path, position);
+}
+
+GameObject* Scene::SpawnObject(std::string path, Vector3 position, Quaternion rotation)
+{
+    return SpawnGameObject(path, position, rotation);
+}
+
+GameObject* Scene::SpawnObject(std::string path, Vector3 position, Vector3 eulerRotation)
+{
+    return SpawnGameObject(path, position, eulerRotation);
+}
+
+GameObject* Scene::SpawnObject(GameObject* gameObject)
+{
+    return SpawnGameObject(gameObject);
+}
+
+GameObject* Scene::SpawnObject(GameObject* gameObject, Vector3 position)
+{
+    return SpawnGameObject(gameObject, position);
+}
+
+GameObject* Scene::SpawnObject(GameObject* gameObject, Vector3 position, Quaternion rotation)
+{
+    return SpawnGameObject(gameObject, position, rotation);
+}
+
+GameObject* Scene::SpawnObject(GameObject* gameObject, Vector3 position, Vector3 eulerRotation)
+{
+    return SpawnGameObject(gameObject, position, eulerRotation);
 }
--- CryonicEngine/Scenes/Scene.h
+++ CryonicEngine/Scenes/Scene.h
@@ -13,10 +13,162 @@ public:
 
     std::filesystem::path GetPath();
     void SetPath(std::filesystem::path path);
+
     GameObject* AddGameObject(int id = 0);
     void RemoveGameObject(GameObject* gameObject);
     std::deque<GameObject*>& GetGameObjects();
     GameObject* GetGameObject(const std::string& name); 
+
+    /**
+     * Spawns a game object from a sprite/template file at a specified position and rotation (Quaternion).
+     *
+     * @param path [std::string] - Relative path to the sprite/template file.
+     * @param position [Vector3] - The world position to spawn the object at.
+     * @param rotation [Quaternion] - The world rotation of the object as a quaternion.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnGameObject(std::string path, Vector3 position, Quaternion rotation);
+
+    /**
+     * Spawns a game object from a sprite/template file.
+     *
+     * @param path [std::string] - Relative path to the sprite/template file.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnGameObject(std::string path);
+
+    /**
+     * Spawns a game object from a sprite/template file at a specified position.
+     *
+     * @param path [std::string] - Relative path to the sprite/template file.
+     * @param position [Vector3] - The world position to spawn the object at.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnGameObject(std::string path, Vector3 position);
+
+    /**
+     * Spawns a game object from a sprite/template file at a specified position and rotation (Euler angle in degrees).
+     *
+     * @param path [std::string] - Relative path to the sprite/template file.
+     * @param position [Vector3] - The world position to spawn the object at.
+     * @param rotation [Vector3] - The world rotation of the object as an euler angle in degrees.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnGameObject(std::string path, Vector3 position, Vector3 rotation);
+
+    /**
+     * Spawns a copy of an existing game object at a specified position and rotation (Quaternion).
+     *
+     * @param gameObject [GameObject*] - A pointer to the game object to clone..
+     * @param position [Vector3] - The world position to spawn the cloned object at.
+     * @param rotation [Quaternion] - The world rotation of the cloned object as a quaternion.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnGameObject(GameObject* gameObject, Vector3 position, Quaternion rotation);
+
+    /**
+     * Spawns a copy of an existing game object.
+     *
+     * @param gameObject [GameObject*] - A pointer to the game object to clone.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnGameObject(GameObject* gameObject);
+
+    /**
+     * Spawns a copy of an existing game object at a specified position.
+     *
+     * @param gameObject [GameObject*] - A pointer to the game object to clone.
+     * @param position [Vector3] - The world position to spawn the cloned object at.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnGameObject(GameObject* gameObject, Vector3 position);
+
+    /**
+     * Spawns a copy of an existing game object at a specified position and rotation (Euler angle in degrees).
+     *
+     * @param gameObject [GameObject*] - A pointer to the game object to clone..
+     * @param position [Vector3] - The world position to spawn the cloned object at.
+     * @param rotation [Vector3] - The world rotation of the cloned object as an euler angle in degrees.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnGameObject(GameObject* gameObject, Vector3 position, Vector3 rotation);
+
+
+    /**
+     * Alias for SpawnGameObject(): Spawns a game object from a sprite/template file.
+     *
+     * @param path [std::string] - Relative path to the sprite/template file.
+     * @return [GameObject*] - A pointer to the created game object.
+     * 
+     */
+    GameObject* SpawnObject(std::string path);
+
+    /**
+     * Alias for SpawnGameObject(): Spawns a game object from a sprite/template file at a specified position.
+     *
+     * @param path [std::string] - Relative path to the sprite/template file.
+     * @param position [Vector3] - The world position to spawn the object at.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnObject(std::string path, Vector3 position);
+
+    /**
+     * Alias for SpawnGameObject(): Spawns a game object from a sprite/template file at a specified position and rotation (Quaternion).
+     *
+     * @param path [std::string] - Relative path to the sprite/template file.
+     * @param position [Vector3] - The world position to spawn the object at.
+     * @param rotation [Quaternion] - The world rotation of the object as a quaternion.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnObject(std::string path, Vector3 position, Quaternion rotation);
+
+    /**
+     * Alias for SpawnGameObject(): Spawns a game object from a sprite/template file at a specified position and rotation (Euler angle in degrees).
+     *
+     * @param path [std::string] - Relative path to the sprite/template file.
+     * @param position [Vector3] - The world position to spawn the object at.
+     * @param rotation [Vector3] - The world rotation of the object as an euler angle in degrees.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnObject(std::string path, Vector3 position, Vector3 rotation);
+
+    /**
+     * Alias for SpawnGameObject(): Spawns a copy of an existing game object.
+     *
+     * @param gameObject [GameObject*] - A pointer to the game object to clone.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnObject(GameObject* gameObject);
+
+    /**
+     * Alias for SpawnGameObject(): Spawns a copy of an existing game object at a specified position.
+     *
+     * @param gameObject [GameObject*] - A pointer to the game object to clone.
+     * @param position [Vector3] - The world position to spawn the cloned object at.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnObject(GameObject* gameObject, Vector3 position);
+
+    /**
+     * Alias for SpawnGameObject(): Spawns a copy of an existing game object at a specified position and rotation (Quaternion).
+     *
+     * @param gameObject [GameObject*] - A pointer to the game object to clone..
+     * @param position [Vector3] - The world position to spawn the cloned object at.
+     * @param rotation [Quaternion] - The world rotation of the cloned object as a quaternion.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnObject(GameObject* gameObject, Vector3 position, Quaternion rotation);
+
+    /**
+     * Alias for SpawnGameObject(): Spawns a copy of an existing game object at a specified position and rotation (Euler angle in degrees).
+     *
+     * @param gameObject [GameObject*] - A pointer to the game object to clone..
+     * @param position [Vector3] - The world position to spawn the cloned object at.
+     * @param rotation [Vector3] - The world rotation of the cloned object as an euler angle in degrees.
+     * @return [GameObject*] - A pointer to the created game object.
+     */
+    GameObject* SpawnObject(GameObject* gameObject, Vector3 position, Vector3 rotation);
+
 
     bool operator==(const Scene& other) const {
         return m_Path == other.m_Path;
