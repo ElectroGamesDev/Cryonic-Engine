ade50457f971b3d55daf2c63f7b3f8ccad2a0bcc
Electro Games <2cool4u567@gmail.com>
2024-05-22 19:43:58 -0500

    Added AudioPlayer

---
+++ CryonicEngine/Components/AudioPlayer.cpp
@@ -0,0 +1,165 @@
+#include "AudioPlayer.h"
+
+void AudioPlayer::Start()
+{
+}
+
+void AudioPlayer::Update(float deltaTime)
+{
+	if (audioClip != nullptr && audioLoaded && !audioClip->ShouldLoadInMemory())
+		Raylib::UpdateMusicStream(music);
+
+	for (auto it = musicStreams.begin(); it != musicStreams.end();)
+	{
+		if (Raylib::IsMusicReady(*it) && !Raylib::IsMusicStreamPlaying(*it)) // Removes the music from the vector if its finished playing
+			it = musicStreams.erase(it);
+		else
+		{
+			Raylib::UpdateMusicStream(*it);
+			++it;
+		}
+	}
+}
+
+void AudioPlayer::Destroy()
+{
+	Stop();
+
+	for (Raylib::Music& musicStream : musicStreams)
+		Raylib::StopMusicStream(musicStream);
+
+	if (this->audioClip != nullptr)
+		delete this->audioClip;
+}
+
+void AudioPlayer::SetAudioClip(AudioClip audioClip)
+{
+	if (this->audioClip != nullptr)
+	{
+		Stop();
+		delete this->audioClip;
+	}
+
+	if (audioClip.GetPath().empty() || audioClip.GetPath() == "nullptr") // It may be "nullptr" if no audio clip was set in the properties
+	{
+		ConsoleLogger::WarningLog(gameObject->GetName() + ":Audio Player - SetAudioClip(AudioClip audioClip) failed to set audio clip. The selected audio clip has an invalid audio file path.", false);
+		return;
+	}
+
+	this->audioClip = new AudioClip(audioClip.GetPath());
+}
+
+bool AudioPlayer::IsPlaying() const
+{
+	if (audioClip == nullptr || !audioLoaded)
+		return false;
+
+	if (audioClip->ShouldLoadInMemory())
+		return Raylib::IsSoundPlaying(sound);
+	else
+		return Raylib::IsMusicStreamPlaying(music);
+}
+
+void AudioPlayer::Play()
+{
+	if (audioClip == nullptr)
+	{
+		ConsoleLogger::WarningLog(gameObject->GetName() + ":Audio Player - Play() failed to play audio. No audio clip selected. Use SetAudioClip() before Play() or Play(AudioClip audioClip).", false);
+		return;
+	}
+	std::filesystem::path path = std::filesystem::path(RaylibWrapper::GetWorkingDirectory()) / "Resources" / "Assets" / audioClip->GetPath();
+	if (audioClip->GetPath().empty() || audioClip->GetPath() == "nullptr" || !std::filesystem::exists(path)) // It may be "nullptr" if no audio clip was set in the properties
+	{
+		ConsoleLogger::WarningLog(gameObject->GetName() + ":Audio Player - Play() failed to play audio. The selected audio clip has an invalid audio file path.", false);
+		return;
+	}
+
+	if (audioClip->ShouldLoadInMemory())
+	{
+		// Todo: Add this
+		// When should they be unloaded, game close?
+		Raylib::PlaySound(sound); // Does it need to wait until sound is loaded to call this?
+	}
+	else
+	{
+		music = Raylib::LoadMusicStream(path.string().c_str());
+		music.looping = loop;
+		Raylib::PlayMusicStream(music);
+	}
+	audioLoaded = true;
+	paused = false;
+}
+
+void AudioPlayer::Play(AudioClip audioClip)
+{
+	std::filesystem::path path = std::filesystem::path(RaylibWrapper::GetWorkingDirectory()) / "Resources" / "Assets" / audioClip.GetPath();
+	if (audioClip.GetPath().empty() || audioClip.GetPath() == "nullptr" || !std::filesystem::exists(path)) // It may be "nullptr" if no audio clip was set in the properties
+	{
+		ConsoleLogger::WarningLog(gameObject->GetName() + ":Audio Player - Play(AudioClip audioClip) failed to play audio. The selected audio clip has an invalid audio file path.", false);
+		return;
+	}
+	// Todo: Ensure audio clip works and send errror if not (like check if the path exists)
+
+	if (audioClip.ShouldLoadInMemory())
+	{
+		// Todo: Add this
+		// When should they be unloaded, game close?
+		//Raylib::PlaySound(); // Does it need to wait until sound is loaded to call this?
+	}
+	else
+	{
+		musicStreams.push_back(Raylib::LoadMusicStream((std::filesystem::path(RaylibWrapper::GetWorkingDirectory()) / "Resources" / "Assets" / audioClip.GetPath()).string().c_str()));
+		Raylib::PlayMusicStream(music);
+	}
+}
+
+void AudioPlayer::Stop()
+{
+	if (!IsPlaying())
+		return;
+
+	if (audioClip->ShouldLoadInMemory())
+		Raylib::StopSound(sound);
+	else
+		Raylib::StopMusicStream(music);
+	audioLoaded = false;
+}
+
+void AudioPlayer::Pause()
+{
+	paused = true;
+	if (audioClip == nullptr)
+		return;
+
+	if (audioClip->ShouldLoadInMemory())
+		Raylib::PauseSound(sound);
+	else
+		Raylib::PauseMusicStream(music);
+}
+
+void AudioPlayer::Unpause()
+{
+	paused = false;
+	if (audioClip == nullptr)
+		return;
+
+	if (audioClip->ShouldLoadInMemory())
+		Raylib::ResumeSound(sound);
+	else
+		Raylib::ResumeMusicStream(music);
+}
+
+bool AudioPlayer::IsPaused() const
+{
+	return paused;
+}
+
+void AudioPlayer::SetLooping(bool loop)
+{
+	this->loop = loop;
+}
+
+bool AudioPlayer::IsLooping() const
+{
+	return loop;
+}
---
+++ CryonicEngine/Components/AudioPlayer.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "Component.h"
+#include "../AudioClip.h"
+#include <vector>
+namespace Raylib
+{
+#include "raylib.h"
+}
+
+class AudioPlayer : public Component
+{
+public:
+    AudioPlayer(GameObject* obj, int id) : Component(obj, id)
+	{
+		name = "AudioPlayer";
+		iconUnicode = "\xef\x9c\x8c";
+
+#if defined(EDITOR)
+        std::string variables = R"(
+        [
+            0,
+            [
+                [
+                    "AudioClip",
+                    "audioClip",
+                    "nullptr",
+                    "Audio Clip",
+                    {
+                        "Extensions": [".mp3", ".wav", ".ogg", ".flac", ".qoa", ".xm", ".mod"]
+                    },
+                    [
+                        "bool",
+                        "loop",
+                        false,
+                        "Loop"
+                    ]
+                ]
+            ]
+        ]
+    )";
+        exposedVariables = nlohmann::json::parse(variables);
+#endif
+	}
+
+	void Start() override;
+	void Update(float deltaTime) override;
+
+    bool IsPlaying() const;
+    void Play();
+    void Play(AudioClip audioClip);
+    void Stop();
+    void Pause();
+    void Unpause();
+    bool IsPaused() const;
+    void SetLooping(bool loop);
+    bool IsLooping() const;
+
+    void Destroy() override;
+
+    // Get clip length -- Should this be in AudioClip? But then audioClip would need to be puclic or create a GetAudioClip() const. ALthough people should have their audioclip unless they dragged it into AudioPlayer.
+    //   It should stay private since SetAudioClip() does important stuff
+    // Get current time elapsed?
+
+    void SetAudioClip(AudioClip audioClip);
+
+private:
+    bool audioLoaded = false; // Whether sound or music is loaded
+    Raylib::Music music; // Used for streaming sounds
+    Raylib::Sound sound; // Used for sounds in memory
+    std::vector<Raylib::Music> musicStreams; // Used for Play(audioClip)
+    AudioClip* audioClip = nullptr;
+    bool loop = false;
+    bool paused = false;
+    // StartOnAwake bool (make sure actually starts). Also if it truies starting on awake with no audioClip, don't call Play() so it wont send warning
+};
