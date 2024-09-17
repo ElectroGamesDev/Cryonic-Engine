bedf1358fc01ec032bdc0b4a492246069bfe2e6b
Electro Games <2cool4u567@gmail.com>
2024-09-17 00:29:50 -0500

    Created Label UI Component

---
+++ CryonicEngine/Components/Label.cpp
@@ -0,0 +1,82 @@
+#include "Label.h"
+#include "../RaylibDrawWrapper.h"
+#include "../RaylibWrapper.h"
+#include "CameraComponent.h"
+#if defined (EDITOR)
+#include "imgui.h"
+#else
+#include "../imgui.h"
+#endif
+
+void Label::Start()
+{
+    // Todo: Move this to Awake
+#if defined(EDITOR)
+    font = new Font(exposedVariables[1][2][2].get<std::string>()); // Todo: Handle if the path no longer exists
+#endif
+    SetFont(font);
+}
+
+void Label::RenderGui()
+{
+    ImGui::PushFont(FontManager::GetFont(font->GetPath(), fontSize, false));
+    Vector2 screenPosition = CameraComponent::main->GetWorldToScreen(gameObject->transform.GetPosition());
+
+#if defined(EDITOR)
+    // Divding positions by Raylib window size then multiply it by Viewport window size
+    screenPosition.x = screenPosition.x / RaylibWrapper::GetScreenWidth() * ImGui::GetWindowSize().x;
+    screenPosition.y = screenPosition.y / RaylibWrapper::GetScreenHeight() * ImGui::GetWindowSize().y;
+#endif
+
+    // Center the text to the game object
+    ImVec2 textSize = ImGui::CalcTextSize(text.c_str());
+    screenPosition.x -= textSize.x / 2;
+    screenPosition.y -= textSize.y / 2 - 10;
+
+    ImGui::SetCursorPos({ screenPosition.x, screenPosition.y });
+    ImGui::TextColored({(float)color.r / 255, (float)color.g / 255, (float)color.b / 255, (float)color.a / 255}, text.c_str());
+    ImGui::PopFont();
+}
+
+#if defined(EDITOR)
+void Label::EditorUpdate()
+{
+    static bool setup = false;
+    if (!setup)
+    {
+        Start();
+        setup = true;
+    }
+
+    color.r = exposedVariables[1][1][2][0].get<int>();
+    color.g = exposedVariables[1][1][2][1].get<int>();
+    color.b = exposedVariables[1][1][2][2].get<int>();
+    color.a = exposedVariables[1][1][2][3].get<int>();
+
+    text = exposedVariables[1][0][2].get<std::string>();
+}
+#endif
+
+void Label::SetText(std::string text)
+{
+    this->text = text;
+}
+std::string Label::GetText() const
+{
+    return text;
+}
+void Label::SetFontSize(int size)
+{
+    fontSize = size;
+    FontManager::LoadFont(font->GetPath(), fontSize, false, true);
+}
+int Label::GetFontSize() const
+{
+    return fontSize;
+}
+
+void Label::SetFont(Font* font)
+{
+    this->font = font;
+    SetFontSize(fontSize);
+}
---
+++ CryonicEngine/Components/Label.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "Component.h"
+#include "../CryonicCore.h"
+#include "../Font.h"
+#include "../FontManager.h"
+
+class Label : public Component
+{
+public:
+	Label(GameObject* obj, int id) : Component(obj, id)
+	{
+		runInEditor = true;
+		name = "Label";
+		iconUnicode = "\xef\x80\xb1";
+
+#if defined(EDITOR)
+		std::string variables = R"(
+        [
+            0,
+            [
+                [
+                    "string",
+                    "text",
+                    "",
+                    "Text"
+                ],
+                [
+                    "Color",
+                    "color",
+                    [ 255, 255, 255, 255 ],
+                    "Color"
+                ],
+                [
+                    "Font",
+                    "font",
+                    "nullptr",
+                    "Font",
+                    {
+                        "Extensions": [".ttf", ".otf"]
+                    }
+                ],
+                [
+                    "int",
+                    "fontSize",
+                    16,
+                    "Font Size"
+                ]
+            ]
+        ]
+    )";
+		exposedVariables = nlohmann::json::parse(variables);
+#endif
+	}
+	Label* Clone() override
+	{
+		return new Label(gameObject, -1);
+	}
+	// Hide everything from API
+	void Start() override;
+	void RenderGui() override;
+#if defined(EDITOR)
+	void EditorUpdate() override;
+#endif
+
+	void SetText(std::string text);
+	std::string GetText() const;
+    void SetFontSize(int size);
+    int GetFontSize() const;
+    void SetFont(Font* font);
+
+private:
+	Color color = { 255, 255, 255, 255 };
+	std::string text;
+    Font* font = nullptr;
+    int fontSize = 16;
+};
