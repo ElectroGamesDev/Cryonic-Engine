90d49e5ab798588b53bb1d2cd157defd3641e1d6
Electro Games <2cool4u567@gmail.com>
2024-09-17 00:29:03 -0500

    Added Font class

---
+++ CryonicEngine/Font.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include "json.hpp"
+
+class Font
+{
+public:
+	Font(std::string path)
+	{
+//		std::ifstream file;
+//#ifndef EDITOR
+//		file.open(std::filesystem::path(exeParent) / "Resources" / "Assets" / std::string(path + ".data"));
+//#endif
+//		if (!file.is_open())
+//		{
+//			ConsoleLogger::ErrorLog("Font failed to load. Path: " + path);
+//			return;
+//		}
+
+		this->path = path;
+	}
+
+	/**
+	 * @brief Returns the relative path to the font file.
+	 *
+	 * @return [string] The relative path to the font file.
+	 */
+	std::string GetPath() const { return path; };
+
+private:
+	std::string path = "";
+};
