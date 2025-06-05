f0018044e8f7c9bd98780a9d0ffc4d2defefbf0c
Tristyn <tpoast2@wgu.edu>
2025-06-05 17:05:28 -0500

    Added a MenuManger

---
+++ CryonicEngine/MenuManager.cpp
@@ -0,0 +1,78 @@
+#include "MenuManager.h"
+
+namespace MenuManager
+{
+	static std::stack<Menu> openMenus;
+	static Event globalOpenEvent;
+	static Event globalCloseEvent;
+
+	void OpenMenu(GameObject* menu, std::function<void()> closeCallback)
+	{
+		menu->SetActive(true);
+		openMenus.push({ menu, closeCallback });
+
+		if (GetOpenMenusCount() == 1)
+			globalOpenEvent.Invoke();
+	}
+
+	void CloseTopMenu()
+	{
+		if (GetOpenMenusCount() == 0)
+			return;
+
+		Menu& menu = openMenus.top();
+		menu.gameObject->SetActive(false);
+
+		if (menu.closeCallback)
+			menu.closeCallback();
+
+		openMenus.pop();
+
+		if (GetOpenMenusCount() == 0)
+			globalCloseEvent.Invoke();
+	}
+
+	void CloseAllMenus()
+	{
+		if (GetOpenMenusCount() == 0)
+			return;
+
+		while (GetOpenMenusCount() > 0)
+		{
+			Menu& menu = openMenus.top();
+			menu.gameObject->SetActive(false);
+
+			if (menu.closeCallback)
+				menu.closeCallback();
+
+			openMenus.pop();
+		}
+
+		globalCloseEvent.Invoke();
+	}
+
+	int GetOpenMenusCount()
+	{
+		return openMenus.size();
+	}
+
+	void OnFirstMenuOpened(std::function<void()>& callback)
+	{
+		globalOpenEvent.Subscribe(callback);
+	}
+
+	void RemoveOnFirstOpen(std::function<void()>& callback)
+	{
+		globalOpenEvent.Unsubscribe(callback);
+	}
+
+	void OnLastMenuClosed(std::function<void()>& callback)
+	{
+		globalCloseEvent.Subscribe(callback);
+	}
+
+	void RemoveOnLastClose(std::function<void()>& callback)
+	{
+		globalCloseEvent.Unsubscribe(callback);
+	}
+}
---
+++ CryonicEngine/MenuManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stack>
+#include "GameObject.h"
+#include "Event.h"
+
+namespace MenuManager
+{
+	struct Menu
+	{
+		GameObject* gameObject;
+		std::function<void()> closeCallback;
+	};
+
+	void OpenMenu(GameObject* menu, std::function<void()> closeCallback = nullptr);
+	void CloseTopMenu();
+	void CloseAllMenus();
+	int GetOpenMenusCount();
+
+	// Only runs when the first menu is opened
+	void OnFirstMenuOpened(std::function<void()>& callback);
+	void RemoveOnFirstOpen(std::function<void()>& callback);
+
+	// Only runs when the last menu is closed
+	void OnLastMenuClosed(std::function<void()>& callback);
+	void RemoveOnLastClose(std::function<void()>& callback);
+};
