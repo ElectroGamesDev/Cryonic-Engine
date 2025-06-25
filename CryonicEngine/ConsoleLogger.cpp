560a88ed8701ddc1c939c4ffdda016894d830d4b
ElectroGamesDev <2cool4u567@gmail.com>
2025-06-25 17:25:36 -0500

    Added a limit to console logs

--- CryonicEngine/ConsoleLogger.cpp
+++ CryonicEngine/ConsoleLogger.cpp
@@ -5,19 +5,26 @@
 #include <windows.h>
 #endif
 
+int ConsoleLogger::maxLogs = 1000; // Todo: Make this configurable in settings
 bool ConsoleLogger::showDebugMessages = true;
-std::vector<std::pair<std::string, ConsoleLogger::ConsoleLogType>> ConsoleLogger::logs;
+std::vector<ConsoleLogger::LogEntry> ConsoleLogger::logs(maxLogs);
+int ConsoleLogger::logCount = 0;
+int ConsoleLogger::logStart = 0;
 
 void ConsoleLogger::InfoLog(const std::string& message, bool devMessage)
 {
-    if (devMessage && !showDebugMessages) return;
+    if (devMessage && !showDebugMessages)
+        return;
+
     std::cout << "[INFO] " << message << std::endl;
-    logs.push_back({ message, ConsoleLogType::INFO });
+    PushLog(message, LogType::INFO);
 }
 
 void ConsoleLogger::WarningLog(const std::string& message, bool devMessage)
 {
-    if (devMessage && !showDebugMessages) return;
+    if (devMessage && !showDebugMessages)
+        return;
+
 #ifdef _WIN32
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, 14); // Set color to yellow
@@ -26,12 +33,15 @@ void ConsoleLogger::WarningLog(const std::string& message, bool devMessage)
 #ifdef _WIN32
     SetConsoleTextAttribute(hConsole, 15); // Reset color
 #endif
-    logs.push_back({ message, ConsoleLogType::WARNING });
+
+    PushLog(message, LogType::WARNING);
 }
 
 void ConsoleLogger::ErrorLog(const std::string& message, bool devMessage)
 {
-    if (devMessage && !showDebugMessages) return;
+    if (devMessage && !showDebugMessages)
+        return;
+
 #ifdef _WIN32
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, 12); // Set color to red
@@ -40,5 +50,34 @@ void ConsoleLogger::ErrorLog(const std::string& message, bool devMessage)
 #ifdef _WIN32
     SetConsoleTextAttribute(hConsole, 15); // Reset color
 #endif
-    logs.push_back({ message, ConsoleLogType::ERROR_ });
+
+    PushLog(message, LogType::ERROR_);
+}
+
+
+void ConsoleLogger::PushLog(const std::string& message, LogType type)
+{
+    int index = (logStart + logCount) % maxLogs;
+
+    logs[index] = { message, type };
+
+    if (logCount < maxLogs)
+        ++logCount;
+    else
+        logStart = (logStart + 1) % maxLogs;
+}
+
+const std::vector<ConsoleLogger::LogEntry>& ConsoleLogger::GetLogs()
+{
+    return logs;
+}
+
+int ConsoleLogger::GetLogCount()
+{
+    return logCount;
+}
+
+const ConsoleLogger::LogEntry& ConsoleLogger::GetLog(int index)
+{
+    return logs[(logStart + index) % maxLogs];
 }
--- CryonicEngine/ConsoleLogger.h
+++ CryonicEngine/ConsoleLogger.h
@@ -7,20 +7,33 @@
 class ConsoleLogger
 {
 public:
-    static void InfoLog(const std::string& message, bool devMessage = true);
-    static void WarningLog(const std::string& message, bool devMessage = true);
-    static void ErrorLog(const std::string& message, bool devMessage = true);
-
-    enum class ConsoleLogType {
+    enum class LogType {
         INFO,
         WARNING,
         ERROR_
     };
-    struct ConsoleLog {
+
+    struct LogEntry {
         std::string message;
-        ConsoleLogType type;
+        LogType type;
     };
 
-    static std::vector<std::pair<std::string, ConsoleLogType>> logs;
+    static void InfoLog(const std::string& message, bool devMessage = true);
+    static void WarningLog(const std::string& message, bool devMessage = true);
+    static void ErrorLog(const std::string& message, bool devMessage = true);
+
+    // GetLogs() has been replaced by GetLog() and GetLogCount() to fix issues with the max log cap. Using this function will return logs in the incorrect order if the log count passes the max (1000 by default).
+    static const std::vector<LogEntry>& GetLogs();
+    static int GetLogCount();
+    static const LogEntry& GetLog(int index);
+
     static bool showDebugMessages;
+
+private:
+    static int logCount;
+    static int logStart;
+    static int maxLogs;
+    static std::vector<LogEntry> logs;
+
+    static void PushLog(const std::string& message, LogType type);
 };
